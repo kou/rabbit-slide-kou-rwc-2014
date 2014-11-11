94eded8469eeaba7549b41eabb35f36fa4a7427b
Kouhei Sutou <kou@clear-code.com>
2014-11-11 23:18:48 +0900

    Simplify

--- theme.rb
+++ theme.rb
@@ -1,4 +1,7 @@
 @x_large_font_size *= 0.8
 @title_slide_title_font_size = @x_large_font_size
 
+@image_timer_image = "mini-usa-taro.png"
+@image_slide_number_image = "mini-alice.png"
+
 include_theme("clear-code")
